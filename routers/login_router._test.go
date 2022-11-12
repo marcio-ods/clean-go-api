1831a906b5522300a35c9cbc0f629a291bef808d
marcio-ods <63021988+marcio-ods@users.noreply.github.com>
2022-11-12 10:32:53 -0300

    ensure LoginRouter returns 500 if an invalid httpRequest is provider

--- routers/login_router._test.go
+++ routers/login_router._test.go
@@ -1,5 +1,7 @@
 package routers
 
+// https://www.youtube.com/watch?v=lvvPlJCMuFw&list=PL9aKtVrF05DyEwK5kdvzrYXFdpZfj1dsG&index=3
+// 13 min
 import (
 	"fmt"
 	"testing"
@@ -9,6 +11,14 @@ type LoginRoute struct {
 }
 
 func (l LoginRoute) route(req HttpRequest) (r HttpRequest) {
+
+	emptyRequest := HttpRequest{}
+	if emptyRequest == req {
+		return HttpRequest{
+			StatusCode: 500,
+		}
+	}
+
 	email, password := req.Body.email, req.Body.password
 	if email == "" || password == "" {
 		return HttpRequest{
@@ -64,3 +74,29 @@ func TestPasswordIsProvided(t *testing.T) {
 	}
 
 }
+
+func TestLoginRoute(t *testing.T) {
+
+	sut := LoginRoute{}
+
+	emptyRequest := HttpRequest{}
+
+	httpResponse := sut.route(emptyRequest)
+
+	if httpResponse.StatusCode != 500 {
+		t.Error("Should return 500 if no  httpRequest is provided")
+	}
+
+}
+
+// func TestLoginRouteNoBody(t *testing.T) {
+// 	// Erro do programador, o Go não compila com objeto inválido
+// 	sut := LoginRoute{}
+
+// 	httpResponse := sut.route(interface{})
+
+// 	if httpResponse.StatusCode != 500 {
+// 		t.Error("Should return 500 if no  httpRequest has no body")
+// 	}
+
+// }
